0dfdf5cf121ba2287aa5c78adc5d7fbaa3e78764
PLEXUS\miguel.generosovaler <miguel.generosovalero@plexus.es>
2024-03-07 14:29:24 +0100

    Mi primer commit

---
+++ src/main/java/com/miguel/proyectoJava/ProyectoJavaApplication.java
@@ -0,0 +1,13 @@
+package com.miguel.proyectoJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/miguel/proyectoJava/ProyectoJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miguel.proyectoJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
